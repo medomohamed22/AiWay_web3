c8957744d34285403492a50bfa57086d2d97dfc7
medomohamed22 <123499261+medomohamed22@users.noreply.github.com>
2026-07-15 22:23:12 +0300

    Delete scripts directory

--- scripts/create-admin-hash.js
+++
@@ -1,4 +0,0 @@
-import { hashPassword } from '../api/_lib.js';
-const password = process.argv[2];
-if (!password) { console.error('Usage: node scripts/create-admin-hash.js "StrongPassword"'); process.exit(1); }
-console.log(hashPassword(password));
